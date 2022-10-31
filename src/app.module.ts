b15644d447eb54218cf588233730a07910161f06
GuidoFC <guidofabiancabrera@hotmail.com>
2022-10-30 21:00:34 -0300

    env config in dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AnimesModule } from './animes/animes.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import config from './config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+    envFilePath: '.env',
+    load: [config],
+    isGlobal: true
+  }),
     MongooseModule.forRoot(
       process.env.DATABASE,
     ),
